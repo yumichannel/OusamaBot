08e1640035c3f1e5c59411d8dbccdb211b969097
Yumi <caubedondoc.ls01@gmail.com>
2018-09-05 12:50:36 +0700

    .

--- ousama.js
+++ ousama.js
@@ -4,6 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const userID = process.env.userID;
+const http = require('http')
 // var config = JSON.parse(fs.readFileSync('config.json','utf8'));
 var config = {
 	status: 'off',
